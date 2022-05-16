a02ac0a8b0cd9a5843f082fdc93a916120ae8bc3
DrSpectacles <78048895+DrSpectacles@users.noreply.github.com>
2022-05-16 17:42:03 +0100

    Update genRdSrfc.m

--- genRdSrfc.m
+++ genRdSrfc.m
@@ -3,6 +3,9 @@ clear;
 %function [out] = genRdSrfc();
 %GENRDSRFC Summary of this function goes here
 %   Detailed explanation goes here
+
+% to add: function needs inputs for timestep and number of steps instead of end time
+% also speed
 speed = 60;
 v = (1/3.6)*speed;
 t = [0:0.001:(20-0.001)];
@@ -21,6 +24,11 @@ for i = 1:10
     outvec(((2000*(i-1)) + 1):(2000*i)) = outvec(((2000*(i-1)) + 1):(2000*i)) + (2/100)*rand*stepvec(((2000*(i-1)) + 1):(2000*i)) ;
 end
     outvec = outvec + 0.05*sin((3/10)*pi*t);
+
+    outvec = [zeros(1,5000), outvec];
+    t = t+5; 
+    tstart = 0:0.001:(5-0.001);
+    t = [tstart, t];
 
     plot(t, outvec);
 
